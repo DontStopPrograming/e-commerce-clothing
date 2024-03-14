ff7aec4c24386b8a7fad4c4bba8379314435215f
DontStopPrograming <01dyamond01@gmail.com>
2024-03-13 21:50:10 -0500

    updating ecommerce and proofreading

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,25 +1,71 @@
+import { useState, useEffect } from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 import './popular.css'
-import data_product from '../assets/data'
 import { Item } from '../Item/Item'
+import data_product from '../assets/data'
 
 export const Popular = () => {
+    const [stateWidth, setStateWidth] = useState(window.innerWidth)
 
+    useEffect(() => {
+        const handleResize = () => {
+            setStateWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
 
     return (
         <div className='popular'>
             <h1> POPULAR IN WOMEN </h1>
             <hr />
 
+            {/* <div className='popular-item'>
 
+                <div className='slide-container'>
+                    {data_product.map((item, i) => (
+
+                        <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+
+                    ))}
+                </div>
+
+
+            </div> */}
 
             <div className='popular-item'>
-                {data_product.map((item, i) => (
 
-                    <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-                ))}
+                {window.innerWidth <= 768 ? (
+                    <div className='slide-container'>
+                        <Carousel showArrows={true} showThumbs={false}>
+                            {data_product.map((item, i) => (
+
+                                <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+
+                            ))}
+                        </Carousel>
+                    </div>
+                ) : (
+                    <div className='slide-container'>
+
+                        {data_product.map((item, i) => (
+
+                            <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+
+                        ))}
+
+                    </div>
+                )}
+
+
 
             </div>
+
+
         </div >
 
     )
