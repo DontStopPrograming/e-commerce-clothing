d3867798e1dab499ae350264dd7c58b6aefee907
DontStopPrograming <01dyamond01@gmail.com>
2024-03-15 19:33:21 -0500

    changed the color - changed of icons for responsive

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -5,10 +5,10 @@ export const Offers = () => {
     return (
         <div className='offers'>
             <div className='offers-left'>
-                <h1> Exclusive </h1>
-                <h1> Offers for you </h1>
+                <h1> Exclusive <br />
+                    Offers for you </h1>
                 <p> ONLY ON BEST SELLERS PRODUCTS </p>
-                <button> Check Now </button>
+                {/* <button> Check Now </button> */}
             </div>
 
             <div className='offers-right'>
