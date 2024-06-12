31f3464630d26478292cf4259bf512cd817d1867
DontStopPrograming <01dyamond01@gmail.com>
2024-06-12 09:23:39 -0500

    fixed: ProductDisplay Component order to design

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -47,7 +47,7 @@ export const ProductDisplay = (props) => {
                     <img src={star_icon} alt='' />
                     <img src={star_icon} alt='' />
                     <img src={star_dull_icon} alt='' />
-                    <p> (122) </p>
+                    {/* <p> (122) </p> */}
                 </div>
 
                 <div className='productDisplay-right-prices'>
