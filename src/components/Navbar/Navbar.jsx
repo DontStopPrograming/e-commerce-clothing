57576c7007a8c8fec109aa37bc64391b7edb2db9
DontStopPrograming <01dyamond01@gmail.com>
2024-06-18 15:55:05 -0500

    Fixed: Navbar changed with colors light

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -107,7 +107,7 @@ export const Navbar = () => {
             ) : (
 
                 <div className='nav-login-cart'>
-                    <Link to='/login'> <button> Login </button> </Link>
+                    <Link to='/login'> <button style={{ color: 'rgb(119, 223, 255)' }}> Login </button> </Link>
                     <Link to='/cart'>  <img src={cart_icon} alt="" /> </Link>
 
                     <div className='nav-cart-count'>
